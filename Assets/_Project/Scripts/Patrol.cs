e91d37c441b1fc4bd155fadb51fffd866b0c4468
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2021-11-04 11:10:59 +0100

    achille-f-patrolTermine

---
+++ Assets/_Project/Scripts/Patrol.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed; 
+    public Transform[] moveSpots;
+    public float startWaitTile;
+    private float waitTime;
+    private int spot;
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+        spot= 0;
+        waitTime = startWaitTile;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[spot].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpots[spot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                spot += 1;
+               spot %= moveSpots.Length;
+                waitTime = startWaitTile;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+        }
+    }
+
+    private int nextSpot()
+    {
+        return spot++ ;
+    }
+}
