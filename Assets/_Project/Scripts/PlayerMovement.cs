12aeed865a60553d453dc73d13eee98b91e3021c
ZERUNEmil <emil.zerun@student.vinci.be>
2021-11-04 14:52:12 +0100

    initial commit adding main character

---
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+    public Animator animator;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+                if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 ||Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical")==-1)
+                {
+                    animator.SetFloat("LastMoveHorizontal", Input.GetAxisRaw("Horizontal"));
+                    animator.SetFloat("LastMoveVertical", Input.GetAxisRaw("Vertical"));
+                
+
+        }
+    }
+    
+    void HandleMovement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+        animator.SetFloat("Horizontal",movement.x);
+        animator.SetFloat("Vertical",movement.y);
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+        
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    
+    
+}
