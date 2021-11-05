2b317081ff4f148604788dbbb7c2034ee3a7abc7
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2021-11-05 10:01:40 +0100

    add GameEnd

---
+++ Assets/EndMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+    public void secretMennu()
+    {
+        SceneManager.LoadScene("GameRules");
+    }
+}
